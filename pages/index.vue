684c5779d676fe1cbe2dad60a0a04878fc4e1bfa
Nguyenthuan <69764143+thuanpham113@users.noreply.github.com>
2021-10-21 02:44:25 +0700

    read API

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,31 @@
 <template>
-  <Tutorial/>
+  <div class="home">
+    <Hero />
+  </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      movie: [],
+    }
+  },
+  async fetch() {
+    await this.getMovies()
+  },
+  methods: {
+    async getMovies() {
+      const data= axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=37ed43a4f8eaa2abd75f9283692947bc&language=en-US&page=1')
+      const result = await data
+      result.data.results.forEach((movie) => {this.movies=(movie)})
+      console.log(this.movies)
+    }
+  }
+}
 </script>
+
+<style>
+</style>
+
